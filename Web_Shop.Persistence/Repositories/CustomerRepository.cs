61d0571e4f209e3d16cfc93f39f3c7de815dab14
Michal.Stys <michal.stys@gmail.com>
2023-12-02 20:56:28 +0100

    Added Customer repository

---
+++ Web_Shop.Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Shop.Persistence.Repositories.Interfaces;
+using WWSI_Shop.Persistence.MySQL.Context;
+using WWSI_Shop.Persistence.MySQL.Model;
+
+namespace Web_Shop.Persistence.Repositories
+{
+    public class CustomerRepository : GenericRepository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(WwsishopContext context) : base(context) { }
+
+        public async Task<bool> EmailExistsAsync(string email)
+        {
+            return await Entities.AnyAsync(e => e.Email == email);
+        }
+
+        public async Task<bool> IsEmailEditAllowedAsync(string email, ulong id)
+        {
+            return !await Entities.AnyAsync(e => e.Email == email && e.IdCustomer != id);
+        }
+
+        public async Task<Customer?> GetByEmailAsync(string email)
+        {
+            return await Entities.FirstOrDefaultAsync(e => e.Email == email);
+        }
+    }
+}
---
+++ Web_Shop.Persistence/Repositories/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using WWSI_Shop.Persistence.MySQL.Model;
+
+namespace Web_Shop.Persistence.Repositories.Interfaces
+{
+    public interface ICustomerRepository : IGenericRepository<Customer>
+    {
+        Task<bool> EmailExistsAsync(string email);
+        Task<bool> IsEmailEditAllowedAsync(string email, ulong id);
+        Task<Customer?> GetByEmailAsync(string email);
+    }
+}
