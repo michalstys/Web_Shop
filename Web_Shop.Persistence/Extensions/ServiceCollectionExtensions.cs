64b48b4e8644005d766ef170e76dc2aa8fbd11b6
Michal.Stys <michal.stys@gmail.com>
2023-12-02 20:46:08 +0100

    path fix

---
+++ Web_Shop.Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Web_Shop.Persistence.MySQL.Extensions;
+
+namespace Web_Shop.Persistence.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMySQLDbContext(configuration);
+        }
+    }
+}
