ff08acb2d07da3e33852d926cbb0ccb3427f2a8b
Michal.Stys <michal.stys@gmail.com>
2024-01-07 22:30:15 +0100

    Interface fix

--- Web_Shop.Application/Services/Interfaces/ICustomerService.cs
+++ Web_Shop.Application/Services/Interfaces/ICustomerService.cs
@@ -11,6 +11,6 @@ namespace Web_Shop.Application.Services.Interfaces
         Task<(bool IsSuccess, Customer? entity, HttpStatusCode StatusCode, string ErrorMessage)> CreateNewCustomerAsync(AddUpdateCustomerDTO dto);
         Task<(bool IsSuccess, Customer? entity, HttpStatusCode StatusCode, string ErrorMessage)> UpdateExistingCustomerAsync(AddUpdateCustomerDTO dto, ulong id);
         Task<(bool IsSuccess, Customer? entity, HttpStatusCode StatusCode, string ErrorMessage)> VerifyPasswordByEmail(string email, string password);
-        Task<(bool IsSuccess, IPagedList<Customer, GetSingleCustomerDTO>? entityList, HttpStatusCode StatusCode, string ErrorMessage)> SearchCustomersAsync(SieveModel paginationParams);
+        //Task<(bool IsSuccess, IPagedList<Customer, GetSingleCustomerDTO>? entityList, HttpStatusCode StatusCode, string ErrorMessage)> SearchCustomersAsync(SieveModel paginationParams);
     }
 }
